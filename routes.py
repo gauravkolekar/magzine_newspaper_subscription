f4e4602d6a54b61107b490a7da486dce54dbe0f3
Gaurav Kolekar <gaurav.kolekar@mavs.uta.edu>
2016-03-28 03:30:03 -0500

    pushing database configuration file and some changes to the routes file

--- routes.py
+++ routes.py
@@ -1,5 +1,19 @@
 from runserver import app
+from database_configuration import database_configuration as db
+#using the database configuration to execute SQL statements
+
+cur = db.cursor()
+# creating a cursor object to execute queries on the database
+
+cur.execute("SELECT VERSION()")
+# query to find out the database version
+
+data = cur.fetchone()
+# Fetch a single row using fetchone() method.
+
+db.close()
+#disconnect from database
 
 @app.route('/')
 def index():
-    return 'Hello World !'
+    return 'Hello World! This website uses mysql: '+str(data)
