fc9be9b08f48d1eefec5f9328a35c171bfb6738c
Gaurav Kolekar <gaurav.kolekar01@gmail.com>
2016-04-20 03:01:16 -0500

    templates added

--- routes.py
+++ routes.py
@@ -1,20 +1,24 @@
-from flask import render_template
+from flask import render_template, request, url_for
 from runserver import app
 from database_configuration import database_configuration as db
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    cur = db.cursor()
-    cur.execute("SELECT VERSION();")
-    data = str(cur.fetchone())
-    #db.close()
-    return render_template('index.html', sqlversion=data)
+    if request.method == 'GET':
+        cur = db.cursor()
+        cur.execute("SELECT VERSION();")
+        data = str(cur.fetchone())
+        #db.close()
+        return render_template('index.html', sqlversion=data)
+    elif request.method == 'POST':
+        user_name = request.form['name']
+        user_address = request.form['address']
+        print user_name, user_address
+        if request.form['submit'] == 'Submit':
+            return render_template('subscription.html')
 
-@app.route('/add_magazine')
+@app.route('/add_magazine', methods=['GET','POST'])
 def add_magazine():
-    #cur = db.cursor()
-    #cur.execute()
-    #data = str(cur.fetchone())
-    #db.close()
-    return render_template('add_magazine.html')
+    if request.method == 'GET':
+        return render_template('add_magazine.html')
