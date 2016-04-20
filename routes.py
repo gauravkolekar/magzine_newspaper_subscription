a77320621ab6159c49bebc8c525f495b535776e5
Gaurav Kolekar <gaurav.kolekar01@gmail.com>
2016-04-20 01:28:07 -0500

    add_magazine.html page added

--- routes.py
+++ routes.py
@@ -1,13 +1,20 @@
 from flask import render_template
 from runserver import app
 from database_configuration import database_configuration as db
-#from database_configuration import cursor as cur
 
 @app.route('/')
 @app.route('/index')
 def index():
     cur = db.cursor()
-    cur.execute("SELECT VERSION()")
+    cur.execute("SELECT VERSION();")
     data = str(cur.fetchone())
-    db.close()
+    #db.close()
     return render_template('index.html', sqlversion=data)
+
+@app.route('/add_magazine')
+def add_magazine():
+    #cur = db.cursor()
+    #cur.execute()
+    #data = str(cur.fetchone())
+    #db.close()
+    return render_template('add_magazine.html')
