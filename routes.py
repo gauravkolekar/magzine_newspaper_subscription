88584bb8b71d132e7633db2057b3cf18b55b9ac4
Gaurav Kolekar <gaurav.kolekar01@gmail.com>
2016-04-20 19:52:34 -0500

    diplaying magazine link by link

--- routes.py
+++ routes.py
@@ -14,11 +14,11 @@ def index():
     elif request.method == 'POST':
         user_name = str(request.form['name'])
         user_address = str(request.form['address'])
-        cur = db.cursor()
-        query = "INSERT INTO CUSTOMER(cname, address) VALUES(user_name,user_address)"
-        print query
-        cur.execute(query)
-        db.commit()
+        #cur = db.cursor()
+        #query = "INSERT INTO CUSTOMER(cname, address) VALUES(user_name,user_address)"
+        #print query
+        #cur.execute(query)
+        #db.commit()
         print user_name, user_address
         if request.form['submit'] == 'Submit':
             return redirect(url_for('subscription'))
@@ -35,10 +35,10 @@ def add_magazine():
         magazine_name = str(request.form['magazine_name'])
         magazine_frequency = str(request.form['magazine_frequency'])
         magazine_editor_name = str(request.form['magazine_editor'])
-        cur = db.cursor()
-        query = "INSERT INTO MAGAZINE VALUES (%s,%s,%s)"%(magazine_name,magazine_frequency,magazine_editor_name)
-        cur.execute(query)
-        db.commit()
+        #cur = db.cursor()
+        #query = "INSERT INTO MAGAZINE VALUES (%s,%s,%s)"%(magazine_name,magazine_frequency,magazine_editor_name)
+        #cur.execute(query)
+        #db.commit()
         print magazine_name, magazine_frequency, magazine_editor_name
         if request.form['submit'] == 'Submit':
             return redirect(url_for('index'))
@@ -47,9 +47,12 @@ def add_magazine():
 def magazines():
     if request.method == 'GET':
         cur = db.cursor()
-        cur.execute("SELECT * FROM MAGAZINE;")
-        data = str(cur.fetchall())
-        return render_template('magazines.html', all_data=data)
+        cur.execute("SELECT m1.pm_name, m1.frequency, m2.state, m2.rate FROM magazine m1,magazine_subscription_rate m2 WHERE m1.pm_name = m2.pm_name;")
+        data = list(cur.fetchall())
+        all_magazines = list()
+        for row in data:
+            all_magazines.append(list(row))
+        return render_template('magazines.html', all_magazines=all_magazines)
 
 @app.route('/daily_newspapers', methods=['GET', 'POST'])
 def daily_newspapers():
