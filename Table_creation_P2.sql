2ce2eb93bfdf0885f6417179a630fda782a9923b
Gaurav Kolekar <gaurav.kolekar01@gmail.com>
2016-04-26 02:02:20 -0500

    styling for index page started

--- Table_creation_P2.sql
+++ Table_creation_P2.sql
@@ -1,11 +1,11 @@
 use subscriptiondb;
 
-DROP TABLE magazine_subscription_rate;
-DROP TABLE daily_newspaper_rate;
-DROP TABLE weekly_newspaper_rate;
 DROP TABLE sub_newspaper_daily;
 DROP TABLE sub_newspaper_weekly;
 DROP TABLE sub_magazine;
+DROP TABLE magazine_subscription_rate;
+DROP TABLE daily_newspaper_rate;
+DROP TABLE weekly_newspaper_rate;
 DROP TABLE newspaper_daily;
 DROP TABLE newspaper_weekly;
 DROP TABLE magazine;
@@ -25,13 +25,22 @@ CREATE TABLE magazine (
     PRIMARY KEY (pm_name)
 );
 
+CREATE TABLE magazine_subscription_rate (
+    pm_name VARCHAR(20),
+    state VARCHAR(20),
+    rate DECIMAL(10 , 5 ),
+    PRIMARY KEY (pm_name, state),
+    FOREIGN KEY (pm_name)
+        REFERENCES magazine (pm_name)
+);
+
 CREATE TABLE sub_magazine (
     id_no INT,
     pm_name VARCHAR(20),
     state VARCHAR(20),
     no_of_issues INT,
     start_date DATE,
-    end_date DATE NOT NULL,   
+    end_date DATE NOT NULL,
     actual_end_date DATE,
     active_flag BOOLEAN,
     cost DECIMAL(10,5),
@@ -42,19 +51,19 @@ CREATE TABLE sub_magazine (
         REFERENCES magazine_subscription_rate (pm_name,state)
 );
 
-CREATE TABLE magazine_subscription_rate (
-    pm_name VARCHAR(20),
-    state VARCHAR(20),
-    rate DECIMAL(10 , 5 ),
-    PRIMARY KEY (pm_name, state),
-    FOREIGN KEY (pm_name)
-        REFERENCES magazine (pm_name)
-);
-
 CREATE TABLE newspaper_daily(
     pn_name VARCHAR(20),
     editor_nd_name VARCHAR(20),
     PRIMARY KEY (pn_name)
+);
+
+CREATE TABLE daily_newspaper_rate (
+    dnr_name VARCHAR(20),
+state VARCHAR(20),
+    rate DECIMAL(10 , 5 ),
+    PRIMARY KEY (dnr_name,state),
+    FOREIGN KEY (dnr_name)
+        REFERENCES newspaper_daily (pn_name)
 );
 
 CREATE TABLE sub_newspaper_daily (
@@ -62,7 +71,7 @@ CREATE TABLE sub_newspaper_daily (
     pnd_name VARCHAR(20),
     state VARCHAR(20),
     no_of_issues INT,
-	sub_type int,
+sub_type int,
     start_date DATE,
     end_date DATE,
     actual_end_date DATE,
@@ -75,15 +84,6 @@ CREATE TABLE sub_newspaper_daily (
         REFERENCES daily_newspaper_rate (dnr_name,state)
 );
 
-CREATE TABLE daily_newspaper_rate (
-    dnr_name VARCHAR(20),
-	state VARCHAR(20),
-    rate DECIMAL(10 , 5 ),
-    PRIMARY KEY (dnr_name,state),
-    FOREIGN KEY (dnr_name)
-        REFERENCES newspaper_daily (pn_name)
-);
-
 
 CREATE TABLE newspaper_weekly(
     pn_name VARCHAR(20),
@@ -91,11 +91,22 @@ CREATE TABLE newspaper_weekly(
     PRIMARY KEY (pn_name)
 );
 
+
+
+CREATE TABLE weekly_newspaper_rate (
+    wnr_name VARCHAR(20),
+state VARCHAR(20),
+    rate DECIMAL(10 , 5 ),
+    PRIMARY KEY (wnr_name,state),
+    FOREIGN KEY (wnr_name)
+        REFERENCES newspaper_weekly (pn_name)
+);
+
 CREATE TABLE sub_newspaper_weekly (
     id_no INT,
     pnw_name VARCHAR(20),
-	state VARCHAR(20),
-	no_of_issues INT,
+state VARCHAR(20),
+no_of_issues INT,
     start_date DATE,
     end_date DATE,
     actual_end_date DATE,
@@ -106,13 +117,4 @@ CREATE TABLE sub_newspaper_weekly (
         REFERENCES customer (id_no),
     FOREIGN KEY (pnw_name,state)
         REFERENCES weekly_newspaper_rate (wnr_name,state)
-);
-
-CREATE TABLE weekly_newspaper_rate (
-    wnr_name VARCHAR(20),
-	state VARCHAR(20),
-    rate DECIMAL(10 , 5 ),
-    PRIMARY KEY (wnr_name,state),
-    FOREIGN KEY (wnr_name)
-        REFERENCES newspaper_weekly (pn_name)
 );
--- Table_insertions.sql
+++ Table_insertions.sql
@@ -3,21 +3,83 @@ INSERT INTO customer(cname, address) VALUES("K.William","102 Norrington West");
 INSERT INTO customer(cname, address) VALUES("F.Rocky","221 Baket st.");
 INSERT INTO customer(cname, address) VALUES("A.Fredrick","104 Bloomington Heights");
 INSERT INTO customer(cname, address) VALUES("S.Smith","104 Bloomington Heights");
+INSERT INTO customer(cname, address) VALUES("L.Mark","101 Random Lane");
+INSERT INTO customer(cname, address) VALUES("K.Justin","102 Criag West");
+INSERT INTO customer(cname, address) VALUES("F.Tucker","221 Baker st.");
+INSERT INTO customer(cname, address) VALUES("A.John","107 Cloomington Heights");
+INSERT INTO customer(cname, address) VALUES("S.Sam","110 Connery St");
+INSERT INTO customer(cname, address) VALUES("L.Root","101 Fischer Lane");
+INSERT INTO customer(cname, address) VALUES("R.William","Philly West");
+INSERT INTO customer(cname, address) VALUES("Cramer","Tim Lane");
+INSERT INTO customer(cname, address) VALUES("Mark","Bloomington Heights");
+INSERT INTO customer(cname, address) VALUES("P.Smith","110 Bloomington Lane");
 
 INSERT INTO magazine (pm_name , frequency, editorm_name) values ("Vogue", "Weekly", "D.Cramer");
 INSERT INTO magazine (pm_name , frequency, editorm_name) values ("NewsToday", "Monthly", "M.White");
+INSERT INTO magazine (pm_name , frequency, editorm_name) values ("Finance", "Yearly", "M.Carrol");
+INSERT INTO magazine (pm_name , frequency, editorm_name) values ("Health", "Monthly", "M.Carl");
+INSERT INTO magazine (pm_name , frequency, editorm_name) values ("AutoCar", "Weekly", "Rachel");
+INSERT INTO magazine (pm_name , frequency, editorm_name) values ("Scientific America", "Weekly", "D.Wenner");
+INSERT INTO magazine (pm_name , frequency, editorm_name) values ("Statistics Dose", "Monthly", "M.Ralph");
+INSERT INTO magazine (pm_name , frequency, editorm_name) values ("Business Standard", "Yearly", "M.Carl");
+INSERT INTO magazine (pm_name , frequency, editorm_name) values ("Diet", "Weekly", "M.Wolf");
+INSERT INTO magazine (pm_name , frequency, editorm_name) values ("AutoCard", "Weekly", "Mitchell");
 
-INSERT INTO magazine_subscription_rate (pm_name, state, rate ) VALUES ("NewsToday" , "Alaska" , 15);
+INSERT INTO magazine_subscription_rate (pm_name, state, rate ) VALUES ("Vogue" , "Alaska" , 15);
 INSERT INTO magazine_subscription_rate (pm_name, state, rate ) VALUES ("NewsToday" , "Alabama" , 15);
-INSERT INTO magazine_subscription_rate (pm_name, state, rate ) VALUES ("NewsToday" , "Texas" , 5);
+INSERT INTO magazine_subscription_rate (pm_name, state, rate ) VALUES ("Finance" , "Texas" , 5);
+INSERT INTO magazine_subscription_rate (pm_name, state, rate ) VALUES ("Health" , "Alabama" , 15);
+INSERT INTO magazine_subscription_rate (pm_name, state, rate ) VALUES ("AutoCar" , "Texas" , 5);
+INSERT INTO magazine_subscription_rate (pm_name, state, rate ) VALUES ("Scientific America" , "Alaska" , 15);
+INSERT INTO magazine_subscription_rate (pm_name, state, rate ) VALUES ("Statistics Dose" , "Alabama" , 15);
+INSERT INTO magazine_subscription_rate (pm_name, state, rate ) VALUES ("Business Standard" , "Texas" , 5);
+INSERT INTO magazine_subscription_rate (pm_name, state, rate ) VALUES ("Diet" , "Alabama" , 15);
+INSERT INTO magazine_subscription_rate (pm_name, state, rate ) VALUES ("AutoCard" , "Texas" , 5);
 
-INSERT INTO sub_magazine (id_no, pm_name, no_of_issues, start_date, end_date, actual_end_date, active_flag, cost ); 
-values (1, "NewsToday", 1, current_date(), '2017-05-19','2017-05-19',1,180);
+INSERT INTO sub_magazine (id_no, pm_name, state, no_of_issues, start_date, end_date, actual_end_date, active_flag, cost ) 
+values (1, "Vogue", "Alaska",1, current_date(), '2017-05-19','2017-05-19',1,10500);
 
-SELECT 
-    m1.pm_name, m1.frequency, m2.state, m2.rate
-FROM
-    magazine m1,
-    magazine_subscription_rate m2
-WHERE
-    m1.pm_name = m2.pm_name    ;
+INSERT INTO sub_magazine (id_no, pm_name, state, no_of_issues, start_date, end_date, actual_end_date, active_flag, cost ) 
+values (2, "NewsToday", "Alabama",1, current_date(), '2017-05-19','2017-05-19',1,5500);
+
+
+INSERT INTO newspaper_daily (pn_name , editor_nd_name) values ("Daily News", "D.Cramer");
+INSERT INTO newspaper_daily (pn_name , editor_nd_name) values ("NewsDaily", "M.White");
+INSERT INTO newspaper_daily (pn_name , editor_nd_name) values ("Finance Times", "M.Carrol");
+
+
+INSERT INTO daily_newspaper_rate (dnr_name, state, rate ) VALUES ("Daily News" , "Alaska" , 15);
+INSERT INTO daily_newspaper_rate (dnr_name, state, rate ) VALUES ("NewsDaily" , "Alabama" , 15);
+INSERT INTO daily_newspaper_rate (dnr_name, state, rate ) VALUES ("Finance Times" , "Texas" , 5);
+
+INSERT INTO sub_newspaper_daily(id_no, pnd_name, state, no_of_issues, start_date, end_date, actual_end_date, active_flag, cost)
+values (3, "Daily News","Alaska",1, current_date(), '2017-05-19','2017-05-19',1,10500);
+
+INSERT INTO sub_newspaper_daily(id_no, pnd_name, state, no_of_issues, start_date, end_date, actual_end_date, active_flag, cost)
+values (4, "NewsDaily","Alabama",1, current_date(), '2017-05-19','2017-05-19',1,5500);
+
+INSERT INTO newspaper_weekly (pn_name , editor_nw_name) values ("Weekly News", "D.Cramer");
+INSERT INTO newspaper_weekly (pn_name , editor_nw_name) values ("NewsWeekly", "M.White");
+INSERT INTO newspaper_weekly (pn_name , editor_nw_name) values ("Roman Times", "M.Carrol");
+
+INSERT INTO weekly_newspaper_rate (wnr_name, state, rate ) VALUES ("Weekly News" , "Alaska" , 15);
+INSERT INTO weekly_newspaper_rate (wnr_name, state, rate ) VALUES ("NewsWeekly" , "Alabama" , 15);
+INSERT INTO weekly_newspaper_rate (wnr_name, state, rate ) VALUES ("Roman Times" , "Texas" , 5);
+
+INSERT INTO sub_newspaper_weekly(id_no, pnw_name, state, no_of_issues, start_date, end_date, actual_end_date, active_flag, cost)
+values (5, "Weekly News","Alaska",1, current_date(), '2017-05-19','2017-05-19',1,10500);
+
+INSERT INTO sub_newspaper_weekly(id_no, pnw_name, state, no_of_issues, start_date, end_date, actual_end_date, active_flag, cost)
+values (6, "NewsWeekly","Alabama",1, current_date(), '2017-05-19','2017-05-19',1,5500); 
+
+select c1.cname Customer_Name, c1.address Customer_Address, s1.pm_name Publication_Name, m1.frequency Frequency, m2.state, m2.rate, s1.end_date End_Date, s1.cost 
+from customer c1, sub_magazine s1, magazine m1, magazine_subscription_rate m2
+ where s1.id_no = c1.id_no and s1.pm_name = m2.pm_name and s1.state = m2.state and m1.pm_name = m2.pm_name;
+
+select c1.cname Customer_Name, c1.address Customer_Address, s1.pnd_name Publication_Name, s1.sub_type Subscription_Type, m2.state, m2.rate, s1.end_date End_Date, s1.cost 
+from customer c1, sub_newspaper_daily s1, newspaper_daily m1, daily_newspaper_rate m2 
+where s1.id_no = c1.id_no and s1.pnd_name = m2.dnr_name and s1.state = m2.state and m1.pn_name = m2.dnr_name; 
+
+select c1.cname Customer_Name, c1.address Customer_Address, s1.pnw_name Publication_Name, s1.state, m2.rate, s1.end_date End_Date, s1.cost 
+from customer c1, sub_newspaper_weekly s1, newspaper_weekly m1, weekly_newspaper_rate m2
+where s1.id_no = c1.id_no and s1.pnw_name = m2.wnr_name and s1.state = m2.state and m1.pn_name = m2.wnr_name;
